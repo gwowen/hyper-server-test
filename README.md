11764ad8163725d8bb336e8499267d38495a4fb5
Gareth <gareth.w.owen@gmail.com>
2020-07-21 23:54:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Messing around with hyper
+=============================================
+
+This is me messing around with code from *Hands-On Microservices With Rust* to explore Rust further, and the [hyper](http://hyper.rs) crate.
+
+You'd probably be better off looking at *Hands-On Microservices With Rust* unless you really like half-baked comments mixed in with your code.
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+/* This is me messing around with code from "Hands-On 
+Microservices With Rust". Nothing original, except possibly
+my own commentary as I understand bits */ 
+use hyper::{Body, Response, Server};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+
+fn main() {
+    // So far, so very basic, just runs and serves
+    // text that says "Rust Microservices"
+    // on localhost
+    let addr = ([127, 0, 0, 1], 8080).into();
+    let builder = Server::bind(&addr);
+    let server = builder.serve(|| {
+        service_fn_ok(|_| {
+            Response::new(Body::from("Rust Microservice"))
+        })
+    });
+    let server = server.map_err(drop);
+    hyper::rt::run(server);
+}
